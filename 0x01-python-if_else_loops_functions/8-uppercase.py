eef451e3142b792a390f6c877603f417d9f15588
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-06-06 16:36:59 -0700

    Print the last digit of a number

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,5 @@
-#!/usr/bin/python3
-def to_uper(character):
-    if ord(character) >= 97 and ord(character) <= 122:
-        return (ord(character) - 32)
-    else:
-        return ord(character)
-def uppercase(str):
-    new = ""
-    for character in str:
-        new += "%c" % to_uper(character)
-        print("{:s}".format(new))
+#!/usr/bin/env python3
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
