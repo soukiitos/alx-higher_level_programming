4ead1e7fedde4177c34c02954b137f195c35014e
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-06-08 16:25:32 -0700

    Print the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -11,8 +11,6 @@ def add_arg(argv):
             add += int(argv[j])
             j += 1
             print("{:d}".format(add))
-
-
 if __name__ == "__main__":
     import sys
     add_arg(sys.argv)
