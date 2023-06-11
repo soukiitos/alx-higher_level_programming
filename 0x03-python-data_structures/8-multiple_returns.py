56b883a93b21b5a172bced5511c49a1f3ba541c1
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-06-11 14:21:19 -0700

    More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    size = len(sentence)
+    if size < 0:
+        return("None")
+    else:
+        first_character = size, sentence[0]
+    return(first_character)
