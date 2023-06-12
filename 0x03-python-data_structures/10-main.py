a96fa67a11d56cf6f4bf22759f38df2127d272c7
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-06-12 16:44:21 -0700

    Find all multiples of 2 in a list

--- 0x03-python-data_structures/10-main.py
+++ 0x03-python-data_structures/10-main.py
@@ -6,8 +6,5 @@ list_result = divisible_by_2(my_list)
 
 i = 0
 while i < len(list_result):
-    if list_result[i]:
-        print("{:d} {:s} divisible by 2".format(my_list[i], "is"))
-    else:
-        print("is not")
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
     i += 1
