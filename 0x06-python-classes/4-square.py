647edb10733ed6b17b6aed0d45902989f5626ddb
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-06-27 17:29:05 -0700

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Define a square'''
+
+
+class Square:
+    '''Initialise the data'''
+    def __init__(self, size=0):
+        self.__size = size
+    '''Return the current square area'''
+    def area(self):
+        return self.__size**2
+    '''Get method'''
+    def size(self):
+        return self.__size
+    '''set method'''
+    def size(self, value):
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
