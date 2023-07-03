841147f08cc69721a1678e7e61f89f677aa60aed
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-07-03 13:29:12 -0700

    Define the number of instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     '''Instantiation with optional width and height'''
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         '''Create instantiation of rectangle
         Args:
