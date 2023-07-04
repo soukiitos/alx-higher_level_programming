1a03b590201ea9c1bd2a9dee671b6cab358f6d6a
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-07-04 09:52:54 -0700

    Return a string “BestSchool” n times the number of the iteration

--- 0x09-python-everything_is_object/100-main.py
+++ 0x09-python-everything_is_object/100-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-magic_string = __import__('100-magic_string').magic_string
+magic_string_fun = __import__('100-magic_string')
 
 for i in range(10):
-    print(magic_string())
+    print(magic_string_fun.magic_string())
