ca593c059efedf0f81d086653e27823517e60c90
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-07-03 17:12:19 -0700

    The N queens puzzle

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,87 +3,70 @@ import sys
 
 
 def init_board(n):
-    board = []
-    [board.append([]) for i in range(n)]
-    [row.append(' ') for i in range(n) for row in board]
-    return(board)
-
-
-def board_deepcopy(board):
-    if isinstance(board, list):
-        return list(map(board_deepcopy, board))
-    return(board)
+    board = [[' '  for i in range(n)] for i in range(n)]
+    return board
 
 
 def get_solution(board):
     solution = []
     for r in range(len(board)):
-        for c in range(len(board)):
-            if board[r][c] == "Q":
-                solution.append([r, c])
-                break
-    return(solution)
+        solution = []
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] == "Q":
+                    solution.append([r, c])
+                    break
+    return solution
 
 
 def xout(board, row, col):
-    for c in range(col + 1, len(board)):
-        board[row][c] = "x"
-    for c in range(col - 1, -1, -1):
-        board[row][c] = "x"
-    for r in range(row + 1, len(board)):
-        board[r][col] = "x"
-    for r in range(row - 1, -1, -1):
-        board[r][col] = "x"
-    c = col + 1
-    for r in range(row + 1, len(board)):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    c = col - 1
-    for r in range(row - 1, -1, -1):
-        if c < 0:
-            break
-        board[r][c]
-        c -= 1
-    c = col + 1
-    for r in range(row - 1, -1, -1):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    c = col - 1
-    for r in range(row + 1, len(board)):
-        if c < 0:
-            break
-        board[r][c] = "x"
-        c -= 1
+    n = len(board)
+    directions = [
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1),
+            (1, 1),
+            (-1, -1),
+            (1, -1),
+            (-1, 1)
+            ]
+    for dx, dy in directions:
+        r, c = row + dx, col + dy
+        while 0 <= r < n and 0 <= c < n:
+            board[r][c] = "x"
+            r += dx
+            c += dy
 
 
 def recursive_solve(board, row, queens, solutions):
-    if queens == len(board):
+    n = len(board)
+    if queens == n:
         solutions.append(get_solution(board))
-        return(solutions)
-    for c in range(len(board)):
+        return solutions
+    for c in range(n):
         if board[row][c] == " ":
-            tmp_board = board_deepcopy(board)
+            tmp_board = [row[:] for row in board]
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
-    return (solutions)
+            solutions = recursive_solve(
+                    tmp_board, row + 1, queens + 1, solutions
+                    )
+    return solutions
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigit() is False:
+    if not sys.argv[1].isdigit():
         print("N must be a number")
         sys.exit(1)
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
+
     board = init_board(int(sys.argv[1]))
     solutions = recursive_solve(board, 0, 0, [])
-    for sol in slutions:
+    for sol in solutions:
         print(sol)
