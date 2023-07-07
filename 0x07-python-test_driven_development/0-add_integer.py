4f62bb76b787c71c8a5d7879f2b31b13dc622adc
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-07-07 16:16:51 -0700

    Test driven development in python

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # 0-add_integer.py
-'''Add 2 integers'''
+'''Define The addition of 2 integers'''
 
 
 def add_integer(a, b=98):
+    '''Add 2 integers'''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
