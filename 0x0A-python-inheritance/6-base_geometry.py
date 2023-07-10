9a13b60bd6d21e8aeddbe6aa650519d525e6dc97
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-07-10 15:07:42 -0700

    Improve BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Define an empty class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''Geometry module BaseGeometry'''
+
+    def area(self):
+        '''Raise an Exception'''
+
+        raise Exception("area() is not implemented")
