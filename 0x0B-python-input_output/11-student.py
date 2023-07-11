7db8e2a822bfb10541629d932fc970f9220f2455
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-07-11 16:13:48 -0700

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Define a student class'''
+
+
+class Student():
+    '''Define a student'''
+    def __init__(self, first_name, last_name, age):
+        '''Drfine a student function by instance attributes:
+        first_name, last_name and age'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    '''Retrieve a dictionary representation of a Student instance'''
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            return {i: j for i, j in self.__dict__.items() if i in attrs}
+        return self.__dict__
+    '''Replace all attributes of the Student instance'''
+    def reload_from_json(self, json):
+        for i, j in json.items():
+            setattr(self, i, j)
