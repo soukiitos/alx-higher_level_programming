85200b8c0bd445af881c291146efa3907d6c4c78
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-07-11 18:50:57 -0700

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 '''Define Log parsing'''
-import sys
-
-
-size = 0
-code = {'200': 0, '301': 0, '400': 0, '401': 0,
-        '403': 0, '404': 0, '405': 0, '500': 0}
-t = 0
 
 
 def print_stats():
@@ -17,20 +10,27 @@ def print_stats():
             print(f"{i}: {j}")
 
 
-try:
-    for line in sys.stdin:
-        line = line.split()
-        if len(line) >= 2:
-            if line[-2] in code:
-                code[line[-2]] += 1
-                t += 1
-                try:
-                    size += int(line[-1])
-                except ValueError:
-                    pass
-        if t % 10 == 0:
-            print_stats()
-    print_stats()
+if __name__ == "__main__":
+    import sys
 
-except KeyboardInterrupt:
-    print_stats()
+    size = 0
+    code = {'200': 0, '301': 0, '400': 0, '401': 0,
+            '403': 0, '404': 0, '405': 0, '500': 0}
+    t = 0
+
+    try:
+        for line in sys.stdin:
+            line = line.split()
+            if len(line) >= 2:
+                if line[-2] in code:
+                    code[line[-2]] += 1
+                    t += 1
+                    try:
+                        size += int(line[-1])
+                    except ValueError:
+                        pass
+            if t % 10 == 0:
+                print_stats()
+        print_stats()
+    except KeyboardInterrupt:
+        print_stats()
