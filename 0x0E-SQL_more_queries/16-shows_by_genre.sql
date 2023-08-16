6c585cf9f95882c449bd25678954792ada0fb89a
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-08-17 00:29:53 +0100

    SQL more queries

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,4 +4,4 @@
 -- Results must be sorted in ascending order by the show title and genre name
 -- The database name will be passed as an argument of the mysql command
 
-SELECT title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id ORDER BY tutle, tv_genres.name;
+SELECT title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tutle, tv_genres.name;
