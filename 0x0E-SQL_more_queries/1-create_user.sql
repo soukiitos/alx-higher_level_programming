8ba1f009b96b3031debb489133e075fa74645faa
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-08-17 00:38:28 +0100

    SQL more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,5 @@
 -- If the user user_0d_1 already exists, the script should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
