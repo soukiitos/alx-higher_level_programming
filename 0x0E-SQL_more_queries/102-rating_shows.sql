549c2cff55ea0546958ebbe213f723967d8a84f2
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-08-17 01:32:13 +0100

    SQL more queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,4 +3,4 @@
 -- Results must be sorted in descending order by the rating
 -- The database name will be passed as an argument of the mysql command
 
-SELECT title, SUM(tv_show_ratings.rate) 'rating' FROM tv_shows JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id ORDER BY rating DESC;
+SELECT title, SUM(tv_show_ratings.rate) 'rating' FROM tv_shows JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id GROUP BY title ORDER BY rating DESC;
