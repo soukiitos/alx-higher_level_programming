05f1b9704e6692af7c7e7dd747cf8316b329e912
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-08-17 01:30:57 +0100

    SQL more queries

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- List all shows from hbtn_0d_tvshows_rate by their rating
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+-- The database name will be passed as an argument of the mysql command
+
+SELECT title, SUM(tv_show_ratings.rate) 'rating' FROM tv_shows JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id ORDER BY rating DESC;
