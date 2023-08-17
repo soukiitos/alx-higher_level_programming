05666da510a913ee404718cb40372c8b29d232ea
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-08-17 01:05:53 +0100

    SQL more queries

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,4 +4,4 @@
 -- Results must be sorted in ascending order by the genre name
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name FROM tv_genres WHERE tv_genres.id NOT IN (SELECT tv_show_genres.genre_id FROM tv_show_genres JOIN tv_show ON tv_shows.id = tv_show_genres.tv_show_id WHERE tv_shows.title = 'Dexter') ORDER BY tv_genres.name;
+SELECT name FROM tv_genres WHERE tv_genres.id NOT IN (SELECT tv_genres.id FROM tv_genres JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id WHERE tv_shows.title = 'Dexter') ORDER BY tv_genres.name;
