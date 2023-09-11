3ac636ed8046e38fa5312e7716565b9aa49ba704
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-09-11 15:46:20 +0100

    Task 2 and 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const numArg = process.argv.length;
-if (numArg === 2)
-{
+const numArg = process.argv.length - 2;
+if (numArg === 0) {
 	console.log('No argument');
-}
-else if (numArg === 3)
-{
+} else if (numArg === 1) {
 	console.log('Argument found');
-}
-else
-{
+} else {
 	console.log('Arguments found');
 }
