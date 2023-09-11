52e14833437f1b82dd4b7a77fc303ae587db88a2
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-09-11 15:26:37 +0100

    Javascript - Warm up README file and first tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,13 @@
 const numArg = process.argv.length - 2;
 if (numArg == 0)
 {
-	console.log("No argument");
+	console.log('No argument');
 }
 else if (numArg == 1)
 {
-	console.log("Argument found");
+	console.log('Argument found');
 }
 else
 {
-	console.log("Arguments found");
+	console.log('Arguments found');
 }
