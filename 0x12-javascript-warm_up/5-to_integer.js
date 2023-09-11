672d2274aca21d1d17e18b3c420996f80b47443d
Soukaina Lachheb <soukainalachheb93@mail.com>
2023-09-11 16:54:36 +0100

    Print An Integer in Javascript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const intnum = parseInt(process.argv[2]);
 if (isNaN(intnum)) {
   console.log('Not a number');
 } else {
-	console.log(`My number: ${intnum}`);
+  console.log(`My number: ${intnum}`);
 }
