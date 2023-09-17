8ccc794dfad90e20181f571bdb406e8834d8be93
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-09-17 02:28:42 +0100

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    con = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    res = cur.fetchall()
+    for i in res:
+        print(i)
+    cur.close()
+    con.close()
