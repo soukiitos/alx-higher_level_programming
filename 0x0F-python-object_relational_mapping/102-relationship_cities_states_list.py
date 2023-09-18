ac9b36b41a454cbe4f5bd403af52d3777c17816a
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-09-18 01:12:56 +0100

    Last tasks

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''List all City objects from the database hbtn_0e_101_usa'''
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_city import Base, City
+from sqlalchemy.orm import sessionmaker, relationship
+from relationship_city import City
+from relationship_state import Base, State
 from sys import argv
 
 
@@ -14,8 +15,10 @@ if __name__ == '__main__':
                 username, password, db_name
                 ), pool_pre_ping=True
             )
-    session = Session(engine)
-    cities = session.query(City).order_by(City.id).all()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(City).order_by(City.id)
     for city in cities:
         s_name = city.s_name
         print("{}: {} -> {}".format(
