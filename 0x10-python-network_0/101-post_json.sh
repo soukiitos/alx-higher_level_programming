3073e4e90b813c9abc158d52f9fa6ba2117fd24b
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-09-28 21:29:40 +0100

    cURL a JSON file

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Send a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s -X POST -H "Content-Type: application/json" --data "@$2" $1
