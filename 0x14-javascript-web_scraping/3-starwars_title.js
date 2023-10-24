224707a2d376eafb408fa7f54a39f5122783df13
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-25 00:34:25 +0100

    javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,14 @@
 const request = require('request');
 const url = process.argv[2];
 const homeworld = 'https://swapi-api.alx-tools.com/api/films/';
+
 request(homeworld + url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode !== 200) {
     console.log('ERROR: ' + response.statusCode);
   } else {
-    resp = JSON.parse(body);
+    const resp = JSON.parse(body);
     console.log(resp.title);
   }
 });
