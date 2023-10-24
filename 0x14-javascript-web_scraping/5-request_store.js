5938d5cd6c2b1ee43bd47bf457e62ea198774d7f
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-24 23:24:20 +0100

    javascript-web_scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require("request");
 const fs = require("fs");
+const request = require("request");
 const url = process.argv[2];
 request(url).pipe(fs.createWriteStream(process.argv[3]));
