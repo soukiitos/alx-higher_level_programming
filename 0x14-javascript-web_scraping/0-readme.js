075ee617ddb9d5b8a55ab6c2253c7c9cfca05760
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-24 22:20:27 +0100

    javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,7 @@
 
 const fs=require('fs');
 
-fs.readFile(process.argv[2],'utf8',function(error, content) {
-	console.log(error || content);
-});	
+fs.readFile(process.argv[2],'utf8',function(error, content)
+	{
+		console.log(error || content);
+	});
