0d27df0b0478672c8f28371159942b33d74f3a73
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-24 22:29:37 +0100

    javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-const fs=require('fs');
-fs.readFileSync(process.argv[2],'utf8',function(error, content) {
-	console.log(error || content);
-});
+const fs = require('fs');
+try {
+	const i = fs.readFileSync(process.argv[2], 'utf8');
+	console.log(i);
+} catch (err_or) {
+	console.error(err_or);
+}
