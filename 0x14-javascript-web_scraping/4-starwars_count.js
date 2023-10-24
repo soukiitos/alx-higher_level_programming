1e39432a7dfa1acc5fe9b6894dce1af2d8ffad65
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-25 00:36:50 +0100

    javascript-web_scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request(url, function(error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
