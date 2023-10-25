6f2fafa1af1e6432f52ac86d0acdbebb19a077ab
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-25 01:36:45 +0100

    javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const urlId = process.argv[2];
+const homeworld = 'https://swapi-api.alx-tools.com/api/films/';
+request.get(homeworld + urlId, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const task = data.characters;
+  for (const i of task) {
+    request.get(i, function (error, response, bo_dy) {
+      if (error) {
+        console.log(error);
+      }
+      const dt = JSON.parse(bo_dy);
+      console.log(dt.name);
+    });
+  }
+});
